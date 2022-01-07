93fae902fb3fd3e1f48b47ac369b5f74d101b14d
Juan Martinez <juanm.jm49@gmail.com>
2022-01-06 23:32:24 -0800

    added splash screen

--- app/src/main/java/com/example/avintura/MainActivity.kt
+++ app/src/main/java/com/example/avintura/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setTheme(R.style.Theme_Avintura_NoActionBar)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
